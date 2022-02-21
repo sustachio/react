b7955616bbf478a146c85b9537ac58c2be3bab19
sustachio <sustachio@gmail.com>
2022-02-21 13:24:38 -0600

    final

--- task-list/src/App.js
+++ task-list/src/App.js
@@ -1,8 +1,10 @@
 import { useState } from "react"
 import Header from './components/Header'
 import Tasks from './components/Tasks'
+import AddTask from "./components/AddTask"
 
 function App() {
+  const [showAddTask, setShowAddTask] = useState(false)
   const [tasks, setTasks] = useState([
     {
         id: 1,
@@ -24,16 +26,34 @@ function App() {
     }
   ])
 
+  // Add Task
+  const addTask = (task) => {
+    const id = Math.floor(Math.random() * 10000) + 1
+    const newTask =  { id, ...task }
+    setTasks([ ...tasks, newTask ])
+  }
+
   // Delete Task
   const deleteTask = (id) => {
     setTasks(tasks.filter((task) => task.id !== id))
   }
 
+  // Toggle reminder
+  const toggleReminder = (id) => {
+    setTasks(
+      tasks.map((task) => 
+        task.id === id ? { ...task, reminder: !task.reminder } : task 
+      )
+    )
+  }
+
   return (
     <div className='container'>
-      <Header title="Hello" />
+      <Header title="Task Tracker" onAdd = {() => setShowAddTask(!showAddTask)} showAdd={showAddTask} />
+      {showAddTask && <AddTask onAdd={addTask}/>}
+
       {tasks.length > 0 ? (
-        <Tasks tasks={tasks} onDelete={deleteTask} />
+        <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
       ) : (
         "no tasks to show"
       )}
